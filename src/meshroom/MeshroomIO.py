f2bda6ec630d7838846be7d6811eacdb08db8e01
Michal Polic <michal.polic@cvut.cz>
2021-08-02 10:52:43 +0200

    Initial commit of baseline pipeline
    The pipeline consist of following steps:
    1) read dense pointcloud in world coordinates from HoloLens depthmaps
    2) run COLMAP to find "correct" camera poses
    3) load COLMAP & load HoloLens camera poses
    4) transform colmap cameras to HoloLens coordinate system
    5) save SfM results to Meshroom SfM JSON file
    6) run Meshroom: undistortion, depthmap estimation, depth filtering,
        meshing with output dense pointcloud
    7) load MVS dense pointcloud
    8) compose common dense pointcloud
    9) filter out noise
    10) calculate the visibility of dense points and save Meshroom SfM
          JSON file
    11) run Meshroom: meshing and texturing
    
    TODO before merging to master:
    1) add HoloLens to colored dense pointcloud mapping in Cpp
    2) add keyframe selection

---
+++ src/meshroom/MeshroomIO.py
@@ -0,0 +1,247 @@
+import os
+import sys
+import numpy as np
+import json
+
+
+class MeshroomIO:
+
+    def mat2str(self, M):
+        '''
+        Print a matrix to JSON
+        '''
+        M = M.reshape(-1)[0]
+        Mstr = [""] * M.size
+        for i in range(0, M.size):
+            Mstr[i] = str(M[0, i])
+        return Mstr
+
+
+    def init_sfm_structure(self, camera):
+        sfm_dict = {
+                "version": ["1", "0", "0"],
+                "featuresFolders": "",
+                "matchesFolders": "",
+                "views": [],
+                "intrinsics": [{
+                    "intrinsicId": str(camera['camera_id']),
+                    "width": str(camera['width']),
+                    "height": str(camera['height']),
+                    "sensorWidth": "1.77777777777777",
+                    "sensorHeight": "1.00000000000000",
+                    "serialNumber": "",
+                    "type": "radial3",
+                    "initializationMode": "estimated",
+                    "pxInitialFocalLength": str(camera['f']),
+                    "pxFocalLength": str(camera['f']),
+                    "principalPoint": [
+                        str(camera['pp'][0]),
+                        str(camera['pp'][1])
+                    ],
+                    "distortionParams": [
+                        str(camera['rd'][0]),
+                        str(camera['rd'][1]),
+                        "0"
+                    ],
+                    "locked": "0"
+                }],
+                "poses": [],
+                "structure": []
+            }
+        return sfm_dict
+
+
+    def add_views_to_sfm_structure(self, sfm_dict, pv_path, images, camera):
+        for img in images:
+            sfm_dict["views"].append({
+                "viewId": str(img['image_id']),
+                "poseId": str(img['image_id']),
+                "intrinsicId": str(img['camera_id']),
+                "path": pv_path + img['name'],
+                "width": str(camera['width']),
+                "height": str(camera['height'])
+            })
+
+            R = img['R']
+            C = img['C']
+            sfm_dict["poses"].append({
+                "poseId": str(img['image_id']),
+                "pose": {
+                    "transform": {
+                        "rotation": self.mat2str(R.T),   # the transpose is because of row-major format
+                        "center": self.mat2str(C)
+                    },
+                    "locked": 0
+                }
+            })
+        
+        return sfm_dict
+
+
+    def transform_images_to_dictionary(self, images):
+        images_dict = {}
+        for img in images:
+            images_dict[str(img['image_id'])] = img
+        return images_dict
+
+
+    def add_points_to_sfm_structure(self, sfm_dict, points, images):
+        images_dict = self.transform_images_to_dictionary(images)
+        for pt in points:
+            if (len(pt['img_pt']) / 2) > 3 and pt['err'] < 2:
+                landmark = {
+                    "landmarkId": str(pt['point3D_id']),
+                    "descType": "sift",
+                    "color": [str(pt['rgb'][0]), str(pt['rgb'][1]), str(pt['rgb'][2])],
+                    "X": [str(pt['X'][0]), str(pt['X'][1]), str(pt['X'][2])],
+                    "observations": []
+                }
+
+                for j in range(int((len(pt['img_pt'])) / 2)):
+                    image_id = int(pt['img_pt'][2*j])
+                    observation_id = int(pt['img_pt'][2*j + 1])
+                    img = images_dict[str(image_id)]
+                    uv = img['uvs'][2*observation_id:2*observation_id+2]
+
+                    landmark["observations"].append({
+                        "observationId": str(image_id),
+                        "featureId": str(observation_id),
+                        "x": [str(uv[0]), str(uv[1])],
+                        "scale": "0"
+                    })
+                sfm_dict["structure"].append(landmark)
+
+        return sfm_dict
+
+
+    def get_obs_hash(self, u, v):
+        return "obs_" + str(u) + "_" + str(v)
+
+
+    def add_xyz_to_sfm_structure(self, sfm_dict, xyz, visibility_map):
+        print("Finding obs for feature point and num. of images.") 
+        max_img_id = 0
+        obs_for_feature = [[] for _ in range(0,np.shape(xyz)[1])]
+        for i in range(0,int(len(visibility_map)/4)):
+            k = visibility_map[4*i]
+            obs_for_feature[k].append(visibility_map[4*i + 1])
+            obs_for_feature[k].append(visibility_map[4*i + 2])
+            obs_for_feature[k].append(visibility_map[4*i + 3])
+            if visibility_map[4*i + 1] > max_img_id: 
+                max_img_id = visibility_map[4*i + 1]
+        
+        print("Setting the observations ids.") 
+        images_obs = [{} for _ in range(0,max_img_id+1)]
+        images_obs_max = [0 for _ in range(0,max_img_id+1)]
+        for i in range(0,int(len(visibility_map)/4)):
+            img_id = visibility_map[4*i + 1]
+            images_obs[img_id][self.get_obs_hash(visibility_map[4*i+2],visibility_map[4*i+3])] = images_obs_max[img_id]
+            images_obs_max[img_id] += 1
+
+        print("Saving the structure.") 
+        for i in range(np.shape(xyz)[1]):
+            if (len(obs_for_feature[i]) / 3) > 3:       # we want at least 3 obs for 3D point
+                landmark = {
+                    "landmarkId": str(i),
+                    "descType": "sift",
+                    "color": ["255", "255", "255"],
+                    "X": [str(xyz[0,i]), str(xyz[1,i]), str(xyz[2,i])],
+                    "observations": []
+                }
+
+                for j in range(int(len(obs_for_feature[i]) / 3)):
+                    img_id = obs_for_feature[i][3*j]    
+                    u = obs_for_feature[i][3*j + 1]
+                    v = obs_for_feature[i][3*j + 2]
+                    if self.get_obs_hash(u,v) in  images_obs[img_id]:
+                        observation_id = images_obs[img_id][self.get_obs_hash(u,v)]
+                        landmark["observations"].append({
+                            "observationId": str(img_id),
+                            "featureId": str(observation_id),
+                            "x": [str(u), str(v)],
+                            "scale": "0"
+                        })
+                sfm_dict["structure"].append(landmark)
+
+        return sfm_dict
+
+
+    def save_colmap_to_json(self, out_path, pv_path, camera, images, points):
+        print("Saving COLMAP to Meshroom JSON.")
+        sfm_dict = self.init_sfm_structure(camera)
+        sfm_dict = self.add_views_to_sfm_structure(sfm_dict, pv_path, images, camera)
+        sfm_dict = self.add_points_to_sfm_structure(sfm_dict, points, images)
+        with open(out_path, 'w') as outfile:
+            json.dump(sfm_dict, outfile)
+
+
+    def save_merged_mvs_to_json(self, out_path, pv_path, camera, images, xyz, visibility_map):
+        print("Saving Holo + MVS to Meshroom JSON.")
+        sfm_dict = self.init_sfm_structure(camera)
+        sfm_dict = self.add_views_to_sfm_structure(sfm_dict, pv_path, images, camera)
+        sfm_dict = self.add_xyz_to_sfm_structure(sfm_dict, xyz, visibility_map)
+        with open(out_path, 'w') as outfile:
+            json.dump(sfm_dict, outfile)
+
+
+    def load_obj_vertices(self, obj_file_path):
+        print("Load OBJ vertices: " + obj_file_path)
+        f = open(obj_file_path, 'r')
+        lines = f.readlines()
+        count = 0
+        for line in lines:
+            if line.startswith('v'):  
+                count += 1
+                continue
+            if line.startswith('f'):
+                break
+
+        count2 = 0
+        xyz = np.zeros((3,count), dtype=float)
+        for line in lines:
+            if line.startswith('v'):    
+                pt_str = line[2:].split(' ')
+                xyz[0][count2] = float(pt_str[0])
+                xyz[1][count2] = float(pt_str[1])
+                xyz[2][count2] = float(pt_str[2])
+                count2 += 1
+                continue
+
+            if line.startswith('f'):
+                break
+
+        f.close()
+        return xyz
+
+    def load_ply_vertices(self, ply_file_path):
+        print("Load PLY vertices: " + ply_file_path)
+        f = open(ply_file_path, 'r')
+        lines = f.readlines()
+        count = 0
+        for line in lines:
+            count += 1
+            if line.startswith('end_header'):  
+                break
+
+        count2 = 0
+        xyz = np.zeros((3,len(lines) - count), dtype=float)
+        for line in lines:
+            count2 += 1
+            if count2 <= count:
+                continue
+            else:
+                line = line.lstrip()
+                line_len = len(line)
+                line = line.replace("  ", " ")
+                while len(line) < line_len:
+                    line_len = len(line)
+                    line = line.replace("  ", " ")
+                    
+                pt_str = line.split(" ")
+
+                xyz[0][count2 - count - 1] = float(pt_str[0])
+                xyz[1][count2 - count - 1]  = float(pt_str[1])
+                xyz[2][count2 - count - 1] = float(pt_str[2]) 
+
+        f.close()
+        return xyz
---
+++ src/utils/UtilsMath.py
@@ -0,0 +1,184 @@
+import ctypes
+import pathlib
+import numpy as np
+from numpy import linalg
+import numpy.matlib
+from scipy.io import savemat
+import scipy.spatial as spatial
+import sys
+sys.path.append('/local/artwin/mapping/codes/mapper/src/utils')
+import renderDepth
+from multiprocessing import Pool
+
+
+class UtilsMath:
+
+    def q2r(self, q):
+        q = q / linalg.norm(np.matrix(q))
+        return np.matrix([
+            [q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3], 2 * (q[1] * q[2] - q[0] * q[3]),
+             2 * (q[1] * q[3] + q[0] * q[2])],
+            [2 * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] - q[1] * q[1] + q[2] * q[2] - q[3] * q[3],
+             2 * (q[2] * q[3] - q[0] * q[1])],
+            [2 * (q[1] * q[3] - q[0] * q[2]), 2 * (q[2] * q[3] + q[0] * q[1]),
+             q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]]])
+
+
+    def r2q(self, R):
+        pass 
+
+
+    def compose_coresponding_camera_centers(self, colmap_cameras, holo_cameras):
+        colmap_C = np.array([]).reshape(3,0)
+        holo_C = np.array([]).reshape(3,0)
+
+        for i in range(0,len(colmap_cameras)):
+            colmap_camera = colmap_cameras[i]
+            holo_camera = holo_cameras[colmap_camera["name"][0:-4]]
+            colmap_C = np.concatenate((colmap_C, colmap_camera["C"]), axis=1)
+            holo_C = np.concatenate((holo_C, holo_camera["C"]), axis=1)
+        
+        return (colmap_C, holo_C)
+
+
+    def estimate_colmap_to_holo_transformation(self, colmap_cameras, holo_cameras):
+        print('Estimate COLMAP to HoloLens transformation.')
+        colmap_C, holo_C = self.compose_coresponding_camera_centers(colmap_cameras, holo_cameras)
+
+        # scale input points
+        mc = np.mean(colmap_C, axis=1)
+        mh = np.mean(holo_C, axis=1)
+        c0 = colmap_C - mc
+        h0 = holo_C - mh
+        normc = np.sqrt(np.sum(np.multiply(c0,c0)))
+        normh = np.sqrt(np.sum(np.multiply(h0,h0)))
+        c0 = c0 * (1/normc)
+        h0 = h0 * (1/normh)
+
+        # find transform. minimize LSQ
+        A = h0 * c0.T
+        U, S, V = linalg.svd(A)     # numpy uses: A = U * np.diag(S) * V
+        R = U * V
+        if linalg.det(R) < 0:
+            V[2,::] = -V[2,::]
+            S[2] = -S[2]
+            R = U * V
+        s = np.sum(S) * normh / normc
+        t = mh - s*R*mc
+
+        return {"scale": s, "rotation": R, "translation": t}
+
+
+    def transform_colmap_points(self, colmap_points, transform):
+        for i in range(0,len(colmap_points)):
+            colmap_points[i]["X"] =  (transform["scale"] * transform["rotation"] * np.matrix(colmap_points[i]["X"]).T + transform["translation"]).T.tolist()[0]
+
+        return colmap_points
+
+    def transform_colmap_images(self, colmap_cameras, transform):
+        for i in range(0,len(colmap_cameras)):
+            colmap_cameras[i]["R"] = colmap_cameras[i]["R"] * transform["rotation"].T
+            colmap_cameras[i]["C"] =  transform["scale"] * transform["rotation"] * colmap_cameras[i]["C"] + transform["translation"]
+
+        return colmap_cameras 
+
+
+    def filter_dense_pointcloud_noise_KDtree(self, xyz, radius, npts):
+        print('Filter noise in dense pointcloud using KDtree.')
+        xyzT = xyz.T
+        point_tree = spatial.cKDTree(xyzT)
+        xyz_filter = []
+        for i in range(0,np.shape(xyz)[1]):
+            pts_ids = point_tree.query_ball_point(xyzT[i,::], radius)
+            if np.max(np.shape(pts_ids)) > npts:
+                xyz_filter.append(True)
+            else:
+                xyz_filter.append(False)
+        
+        xyz = xyz[::,xyz_filter]
+        return xyz
+
+
+    def estimate_visibility_for_image(self, data):
+        img_id = data["image_id"]
+        print(f'Estimate visibility for image: {img_id}')
+        K  = data["K"] 
+        R  = data["R"] 
+        C  = data["C"] 
+        h  = data["h"]
+        w  = data["w"]
+        xyz  = data["xyz"] 
+        t  = data["t"]
+        distance_threshold = data["dt"] 
+        visibility_xyz = [] 
+
+        # project points
+        uvl = K * R * (xyz - C)
+        u = uvl[0,::] / uvl[2,::]
+        v = uvl[1,::] / uvl[2,::]
+        uv = np.concatenate((u, v), axis=0)
+        depth = np.linalg.norm(xyz - C, axis=0)
+
+        # get data in image
+        filter = [(uv[0,j] > 0 and uv[1,j] > 0 and uv[0,j] < w and uv[1,j] < h) for j in range(0,np.shape(uv)[1])]
+        depth_filtered = depth[filter]
+        uv_filtered = uv[::,filter]
+        xyz_filtered = xyz[::,filter]
+        xyz_ids = np.array([j for j in range(0,np.shape(uv)[1])])
+        xyz_ids_filtered = xyz_ids[filter]
+        
+        # sort points wrt. depth
+        depth_order = np.flip(np.argsort(depth_filtered))
+        depth_sorted = depth_filtered[depth_order]
+        uv_sorted = uv_filtered[::,depth_order]
+        xyz_sorted = xyz_filtered[::,depth_order]
+        xyz_ids_sorted = xyz_ids_filtered[depth_order]
+
+        # run cpp to render visibility information
+        holo_depth_img = renderDepth.render(h, w, np.shape(uv_sorted)[1], \
+                np.shape(t)[1], uv_sorted.reshape(1,-1), depth_sorted, t.reshape(1,-1)) 
+        holo_depth_img = holo_depth_img.reshape(h,w)
+        
+        # # test
+        # mdic = {"holo_depth_img": holo_depth_img, "uv": uv_sorted, "depth": depth_sorted, "t": t}
+        # savemat("/local/artwin/mapping/codes/mapper/src/utils/renderDepth/matlab_matrix.mat", mdic)
+
+        # save the visibility information to observations
+        for j in range(0,np.shape(uv_sorted)[1]):
+            loc_uv = np.floor(uv_sorted[::,j]).tolist()
+            loc_depth = depth_sorted[j]
+            if ( np.abs(holo_depth_img[int(loc_uv[1][0]),int(loc_uv[0][0])] - loc_depth) < distance_threshold ):
+                visibility_xyz.append(xyz_ids_sorted[j])
+                visibility_xyz.append(img_id)
+                visibility_xyz.append(loc_uv[0][0])
+                visibility_xyz.append(loc_uv[1][0])
+        
+        return visibility_xyz
+
+
+    def estimate_visibility(self, camera, images, xyz):
+        distance_threshold = 0.1
+        chunksize = 20
+        t = np.array([[8.0, 5.6, 3.2, 0.8, 0],[1, 3, 5, 7, 9]])
+
+        # get visibility estimate
+        w = camera["width"]
+        h = camera["height"]
+        K = np.matrix([[camera["f"], 0, camera["pp"][0]],[0, camera["f"], camera["pp"][1]],[0, 0, 1]])
+        visibility_xyz = [] 
+        
+        all_data = []
+        for image in images:
+            all_data.append({"image_id": image["image_id"], "K": K, "R": image["R"], "C": image["C"], "h": h, "w": w, "xyz": xyz, "t": t, "dt": distance_threshold})
+
+        with Pool() as pool:
+            for ind, res in enumerate(pool.imap(self.estimate_visibility_for_image, all_data), chunksize):
+                for i in range(0,len(res)):
+                    visibility_xyz.append(res[i])       # pt3d_id = res[4*i + 0]    img_id = res[4*i + 1]    u = res[4*i + 2]    v = res[4*i + 3]
+                    
+        return visibility_xyz
+
+
+
+
+        
