c11db4f93508c7080ff38e0d71c2925d522d9311
Michal Polic <michal.polic@cvut.cz>
2022-04-11 11:47:40 +0200

    Add manually HoloLens2ForCV
    The subtree adding does not work. The codes were added manually as
    no modifications of this codes in online repository are expected.

---
+++ third_party/HoloLens2ForCV/Samples/SensorVisualization/SensorVisualization/Content/GyroRenderer.h
@@ -0,0 +1,71 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+#pragma once
+
+#include "..\Common\DeviceResources.h"
+#include "..\Common\StepTimer.h"
+#include "ShaderStructures.h"
+#include "researchmode\ResearchModeApi.h"
+#include <Texture2D.h>
+
+namespace BasicHologram
+{
+    // This sample renderer instantiates a basic rendering pipeline.
+    class GyroRenderer
+    {
+    public:
+        GyroRenderer(std::shared_ptr<DX::DeviceResources> const& deviceResources, IResearchModeSensor *pAccelSensor, HANDLE hasData, ResearchModeSensorConsent *pCamAccessConsent)
+        {
+            m_pGyroSensor = pAccelSensor;
+            m_pGyroSensor->AddRef();
+            m_pAccelUpdateThread = new std::thread(GyroUpdateThread, this, hasData, pCamAccessConsent);
+        }
+        virtual ~GyroRenderer()
+        {
+            m_fExit = true;
+            m_pAccelUpdateThread->join();
+
+            if (m_pGyroSensor)
+            {
+                m_pGyroSensor->CloseStream();
+                m_pGyroSensor->Release();
+            }
+        }
+        void Update(DX::StepTimer const& timer);
+        void UpdateSample();
+
+        // Repositions the sample hologram.
+        void PositionHologram(winrt::Windows::UI::Input::Spatial::SpatialPointerPose const& pointerPose);
+
+        // Property accessors.
+		void SetSensorFrame(IResearchModeSensorFrame* pSensorFrame);
+
+        void GetGyroSample(DirectX::XMFLOAT3 *pGyroSample);
+
+    private:
+        static void GyroUpdateThread(GyroRenderer* pSpinningCube, HANDLE hasData, ResearchModeSensorConsent *pCamAccessConsent);
+        void GyroUpdateLoop();
+
+        // If the current D3D Device supports VPRT, we can avoid using a geometry
+        // shader just to set the render target array index.
+        bool                                            m_usingVprtShaders = false;
+
+		IResearchModeSensor *m_pGyroSensor = nullptr;
+		IResearchModeSensorFrame* m_pSensorFrame;
+        DirectX::XMFLOAT3 m_gyroSample;
+        std::mutex m_sampleMutex;
+
+        std::thread *m_pAccelUpdateThread;
+        bool m_fExit = { false };
+    };
+
+}
---
+++ third_party/HoloLens2ForCV/Samples/StreamRecorder/StreamRecorderConverter/src_convert_images.py
@@ -0,0 +1,71 @@
+"""
+ Copyright (c) Microsoft. All rights reserved.
+ This code is licensed under the MIT License (MIT).
+ THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+ IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+ PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+"""
+import os
+import cv2
+import argparse
+import numpy as np
+import multiprocessing
+from pathlib import Path
+
+from src_utils import folders_extensions
+
+class ConvertImages():
+
+    def write_bytes_to_png(self,bytes_path, width, height):
+        print(".", end="", flush=True)
+
+        output_path = bytes_path.replace('bytes', 'png')
+        if os.path.exists(output_path):
+            return
+
+        image = []
+        with open(bytes_path, 'rb') as f:
+            image = np.frombuffer(f.read(), dtype=np.uint8)
+
+        image = image.reshape((height, width, 4))
+
+        new_image = image[:, :, :3]
+        cv2.imwrite(output_path, new_image)
+
+        # Delete '*.bytes' files
+        os.remove(bytes_path)
+
+
+    def get_width_and_height(self,path):
+        with open(path) as f:
+            lines = f.readlines()
+        (_, _, width, height) = lines[0].split(',')
+
+        return (int(width), int(height))
+
+
+    def convert_images(self,folder):
+        # p = multiprocessing.Pool(multiprocessing.cpu_count())
+        for (img_folder, extension) in folders_extensions:
+            if img_folder == 'PV':
+                pv_path = list(folder.glob('*pv.txt'))
+                assert len(list(pv_path)) == 1 
+                (width, height) = self.get_width_and_height(pv_path[0])
+
+                paths = (folder / img_folder).glob('*bytes')
+                print("Processing images")
+                for path in paths:
+                    #p.apply_async(self.write_bytes_to_png, (str(path), width, height))
+                    self.write_bytes_to_png(str(path), width, height)
+        # p.close()
+        # p.join()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert images')
+    parser.add_argument("--recording_path", required=True,
+                        help="Path to recording folder")
+    args = parser.parse_args()
+    ci = ConvertImages()
+    ci.convert_images(Path(args.recording_path))
