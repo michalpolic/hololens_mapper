e7475157651064596f45067df1a0f1a0954f851c
Michal Polic <michal.polic@cvut.cz>
2022-04-14 08:40:08 +0200

    Fix. bugs in run_hloc.py
    Update the paths and naming for all the functions.

--- third_party/Hierarchical-Localization/run_hloc.py
+++ third_party/Hierarchical-Localization/run_hloc.py
@@ -1,50 +1,58 @@
+import sys
+import os
 from pathlib import Path
 from pprint import pformat
-import sys
-
 from hloc import extract_features, match_features, pairs_from_retrieval
 from hloc import localize_sfm
 
-
-folder = sys.argv[1]
+# input
+map_folder = sys.argv[1]
 query_file = sys.argv[2]
-output = sys.argv[3]
+output_folder = sys.argv[3]
 
-dataset = Path(folder)  # change this if your dataset is somewhere else
-images = dataset
+map_path = Path(map_folder) 
+db_images_path = map_path
+query_folder_path = Path(query_file).parent
+query_images_path = query_folder_path
 
-sfm_pairs = dataset / 'pairs-db-covis20.txt'  # top 20 most covisible in SIFT model
-loc_pairs = dataset / 'pairs-query-netvlad50.txt'  # top 50 retrieved by NetVLAD ?CHYBI?
-
-outputs = Path(output)  # where everything will be saved
-reference_sfm = dataset / 'sfm_superpoint+superglue'  # the SfM model we will build
-results = outputs / 'hololens_hloc_superpoint+superglue_netvlad50.txt'  # the result file
-
-# pick one of the configurations for extraction and matching
-# you can also simply write your own here!
+# setting 
 retrieval_conf = extract_features.confs['netvlad']
 feature_conf = extract_features.confs['superpoint_aachen']
 matcher_conf = match_features.confs['superglue']
 
-feature_path = extract_features.main(feature_conf, images, outputs)
+# database features and descriptors
+reference_sfm = map_path / 'sfm_superpoint+superglue'
+db_feature_path = map_path / 'feats-superpoint-n4096-r1024.h5'
+db_descriptors_path = map_path / 'global-feats-netvlad.h5'
+
+# output paths
+output_path = Path(output_folder)
+query_pairs_path = output_path / 'pairs-query-netvlad50.txt' 
+results_path = output_path / 'superpoint+superglue_netvlad50.txt' 
 
 
-sfm_match_path = dataset / 'feats-superpoint-n4096-r1024_matches-superglue_pairs-db-covis20'
-global_descriptors = extract_features.main(retrieval_conf, images, outputs)
+# ---------------------------------------------------
+#              localization process 
+# ---------------------------------------------------
 
+# detect feature points on query images, extract NetVLAD descriptors for query images
+query_feature_path = extract_features.main(feature_conf, query_images_path, output_path)
+query_descriptors_path = extract_features.main(retrieval_conf, query_images_path, output_path)
 
-pairs_from_retrieval.main(
-    global_descriptors, loc_pairs, 50,
-    query_prefix='query', db_model=reference_sfm)
+# global descriptor matching
+pairs_from_retrieval.main(query_descriptors_path, query_pairs_path, 50, query_prefix='query', \
+    db_model=reference_sfm, db_descriptors = db_descriptors_path)
 
-loc_match_path = match_features.main(matcher_conf, loc_pairs, feature_conf['output'], outputs)
+# feature points matching
+loc_match_path = match_features.main(matcher_conf, query_pairs_path, feature_conf['output'],\
+    output_path, None, db_feature_path)
 
-
+# calculate the poses in global coordinate system
 localize_sfm.main(
     reference_sfm,
     Path(query_file),
-    loc_pairs,
-    feature_path,
+    query_pairs_path,
+    query_feature_path,
     loc_match_path,
-    results,
+    results_path,
     covisibility_clustering=False)  # not required with SuperPoint+SuperGlue
