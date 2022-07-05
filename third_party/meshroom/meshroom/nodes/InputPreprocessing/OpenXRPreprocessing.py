cecf1626d86b0466bf3567ed3afaa9996282d71c
Michal Polic <michal.polic@cvut.cz>
2022-07-05 22:24:01 +0200

    Init the OpenXRPreprocessing node

--- third_party/meshroom/meshroom/nodes/InputPreprocessing/OpenXRPreprocessing.py
+++ third_party/meshroom/meshroom/nodes/InputPreprocessing/OpenXRPreprocessing.py
@@ -12,6 +12,10 @@ import time
 import sys
 import tarfile
 
+import numpy as np
+from PIL import Image
+import xml.etree.ElementTree as ET
+
 # import mapper packages
 dir_path = __file__
 for i in range(6):
@@ -29,12 +33,26 @@ The data are formated such a way to be processed by OI Convertor.
 
     inputs = [
         desc.File(
-            name='recordingsFolder',
-            label='Recordings folder',
-            description='The folder where to download all the recordings.',
+            name='dataFile',
+            label='Data file',
+            description='The file with *.xml data.',
             value='',
             uid=[0],
-        ),        
+        ),
+        desc.File(
+            name='colorFile',
+            label='Color file',
+            description='The file with *color.bin data.',
+            value='',
+            uid=[0],
+        ),
+        desc.File(
+            name='depthFile',
+            label='Depth file',
+            description='The file with *depth.bin data.',
+            value='',
+            uid=[0],
+        ),
         desc.BoolParam(
             name="delete", 
             label="Delete binaries",
@@ -68,18 +86,117 @@ The data are formated such a way to be processed by OI Convertor.
         try:
             chunk.logManager.start(chunk.node.verboseLevel.value)
             
-            if not chunk.node.recordingsFolder:
-                chunk.logger.warning('Missing recordings folder field.')
+            if not chunk.node.dataFile:
+                chunk.logger.warning('Missing data file (*.xml).')
                 return
 
+            if not chunk.node.colorFile:
+                chunk.logger.warning('Missing color data file (*_color.bin).')
+                return
+
+            if not chunk.node.depthFile:
+                chunk.logger.warning('Missing depth data file (*_depth.bin).')
+                return
             # # init
             # holo_io = HoloIO()
             # folder = chunk.node.recordingsFolder.value
             # sensors = ["pv", "vlc_ll", "vlc_lf", "vlc_rf", "vlc_rr", "long_throw_depth"]
             # holo_io.mkdir_if_not_exists(folder, logger = chunk.logger)
 
+        dtype = np.dtype('B')
+        camera_id = "1"
+        root = tree.getroot()
 
-                        
+            # Convertor takes only "depth" or "color" as arguments
+        def Convertor(type):
+            for data in root.findall(type):
+                index = data.get("index")
+                width = data.get("width")
+                height = data.get("height")
+                length = data.get("length")
+                position = data.get("position")
+                
+                width = int(width)
+                height = int(height)
+                length = int(length)
+                position = int(position)
+
+                if type == "color":
+                    with open(clrFile, mode="rb") as file:
+                        numpy_data = np.fromfile(file, dtype, length, sep='', offset=position)
+
+                    array = np.reshape(numpy_data, (height, width, 4))
+                    im = Image.fromarray(array)
+                    im.save(clrPath + "Color" + index + ".png")
+                
+                if type == "depth":
+                    with open(dpthFile, mode="rb") as file:
+                        numpy_data = np.fromfile(file, dtype, length, sep='', offset=position)
+
+                    array = np.reshape(numpy_data, (height, width, 4))
+                    im = Image.fromarray(array)
+                    im.save(dpthPath + "Depth" + index + ".png")
+
+        # Convertor("color")
+        # Convertor("depth")
+
+
+        with open(txtPath + "Pose1.txt", "w+") as f:
+            f.write("#   IMAGE_ID, QW, QX, QY, QZ, TX, TY, TZ, CAMERA_ID, NAME")
+
+            for data in root.findall("pose"):
+                index = data.get("index")
+                
+                tx = data.find("pos").get("x")
+                ty = data.find("pos").get("y")
+                tz = data.find("pos").get("z")
+
+                qw = data.find("rot").get("w")
+                qx = data.find("rot").get("x")
+                qy = data.find("rot").get("y")
+                qz = data.find("rot").get("z")
+
+                f.write("\n" + index +" "+ qw+" "+ qx +" "+ qy +" "+ qz +" "+ tx +" "+ ty +" "+ tz +" "+ camera_id + " Image" + index + ".png")
+
+
+        for data in root.findall("color"):
+            index = data.get("index")
+            width = data.get("width")
+            height = data.get("height")
+            length = data.get("length")
+            position = data.get("position")
+            
+            width = int(width)
+            height = int(height)
+            length = int(length)
+            position = int(position)
+
+            with open(clrFile, mode="rb") as file:
+                numpy_data = np.fromfile(file, dtype, length, sep='', offset=position)
+
+            array = np.reshape(numpy_data, (height, width, 4))
+            im = Image.fromarray(array)
+            im.save(clrPath + "Color" + index + ".png")
+
+
+        for data in root.findall("depth"):
+            index = data.get("index")
+            width = data.get("width")
+            height = data.get("height")
+            length = data.get("length")
+            position = data.get("position")
+            
+            width = int(width)
+            height = int(height)
+            length = int(length)
+            position = int(position)
+
+            with open(dpthFile, mode="rb") as file:
+                numpy_data = np.fromfile(file, dtype, length, sep='', offset=position)
+
+            array = np.reshape(numpy_data, (height, width, 4))
+            im = Image.fromarray(array)
+            im.save(dpthPath + "Depth" + index + ".png")
 
             # delete recordings on device
             if chunk.node.delete.value:
